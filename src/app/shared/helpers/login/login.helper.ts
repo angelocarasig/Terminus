0fd31ef869b232c9c3846b549373e09861782882
Angelo Carasig <s3906344@student.rmit.edu.au>
2023-08-27 21:01:43 +1000

    (BREAKING) init start of promise/observable refactor

--- src/app/shared/helpers/login/login.helper.ts
+++ src/app/shared/helpers/login/login.helper.ts
@@ -1,51 +1,55 @@
-import { Injectable } from '@angular/core';
-import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
-import { firstValueFrom } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {HttpErrorResponse, HttpResponse} from '@angular/common/http';
+import {firstValueFrom, Observable, tap} from 'rxjs';
 
-import { LoginService } from '../../services/login/login.service';
-import { User } from '../../models/user/user';
+import {LoginService} from '../../services/login/login.service';
+import {User} from '../../models/user/user';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({providedIn: 'root'})
 export class LoginHelper {
 
-  constructor(private loginService: LoginService) {
-  }
-
-  async createUser(username: string, authToken: string): Promise<User> {
-    const userDetails = authToken !== ''
-      ? await this.validateAuthToken(username, authToken)
-      : await this.validateUsername(username);
-
-    return {
-      username: userDetails.username,
-      uid: userDetails.id,
-      authToken: {
-        token: authToken,
-        permissions: userDetails?.authToken?.permissions
-      },
-    };
-  }
-
-  private async validateUsername(username: string): Promise<any> {
-    const response = await firstValueFrom(this.loginService.getUser(username));
-    if (response && response[username]) {
-      return response[username];
+    constructor(private loginService: LoginService) {
     }
-    throw new Error(`User '${username}' not found`);
-  }
 
-  private async validateAuthToken(username: string, authToken: string): Promise<any> {
-    return await firstValueFrom(this.loginService.getAuthInfo(authToken))
-      .then(
-        // Safe to assume that a response will contain at least a username
-        (response): HttpResponse<any> => {
-          if (response.username !== username) throw new Error(`Username and the respective auth token does not match!`);
-          return response;
-        },
+    async createUser(username: string, authToken: string): Promise<User> {
+        const userDetails = authToken !== ''
+            ? await this.validateAuthToken(username, authToken)
+            : await this.validateUsername(username);
 
-        (error): HttpErrorResponse => {
-          throw new Error('Auth token was invalid.');
-        }
-      );
-  }
+        return {
+            username: userDetails.username,
+            uid: userDetails.id,
+            authToken: {
+                token: authToken,
+                permissions: userDetails?.authToken?.permissions
+            },
+        };
+    }
+
+    private validateUsername(username: string): Observable<any> {
+        return this.loginService.getUser(username).pipe(
+            tap({
+                next: (response) => {
+                    if (response && response[username]) return response[username];
+                },
+                error: () => {
+                    throw new Error(`User ${username} not found.`);
+                }
+            })
+        );
+    }
+
+    private async validateAuthToken(username: string, authToken: string): Promise<any> {
+        return await firstValueFrom(this.loginService.getAuthInfo(authToken)).then(
+            (response) => {
+                if (response.username !== username) {
+                    throw new Error(`Username and the respective auth token do not match!`);
+                }
+                return response;
+            },
+            (error) => {
+                throw new Error('Auth token was invalid.');
+            }
+        );
+    }
 }
--- src/app/shared/services/login/login.service.ts
+++ src/app/shared/services/login/login.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 
-import { Observable } from 'rxjs';
-import { environment } from '../../../../environments/environment.prod';
+import {Observable} from 'rxjs';
+import {environment} from '../../../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
@@ -13,10 +13,10 @@ export class LoginService {
   }
 
   getUser(username: string): Observable<any> {
-    return this.http.get<any>(`${environment.apiUrl}${environment.endpoints.user}`, { params: new HttpParams().set('q', username) });
+    return this.http.get<any>(`${environment.apiUrl}${environment.endpoints.user}`, {params: new HttpParams().set('q', username)});
   }
 
   getAuthInfo(authToken: string): Observable<any> {
-    return this.http.get<any>(`${environment.apiUrl}${environment.endpoints.authinfo}`, { headers: new HttpHeaders({ Authorization: authToken }) });
+    return this.http.get<any>(`${environment.apiUrl}${environment.endpoints.authinfo}`, {headers: new HttpHeaders({Authorization: authToken})});
   }
 }
